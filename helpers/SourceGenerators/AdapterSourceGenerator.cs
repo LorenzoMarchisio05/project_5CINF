215bb0b3e2d2ed745421137868d75bc42c3b9bef
Lorenzo Marchisio <l.marchisio.2268@vallauri.edu>
2023-10-05 09:09:05 +0200

    init SourceGeneratorProject

---
+++ helpers/SourceGenerators/AdapterSourceGenerator.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SourceGenerators
+{
+    [Generator]
+    public class AdapterSourceGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            /*
+            context.RegisterPostInitializationOutput(ctx =>
+            {
+                var sourceText = $$"""
+                    namespace SourceGeneratorInCSharp
+                    {
+                        public static class HelloWorld
+                        {
+                            public static void SayHello()
+                            {
+                                Console.WriteLine("Hello From Generator");
+                            }
+                        }
+                    }
+                    """;
+                ctx.AddSource("ExampleGenerator.g", SourceText.From(sourceText, Encoding.UTF8));
+            });
+            */
+
+            var typesWithMyAttribute =
+                from a in AppDomain.CurrentDomain.GetAssemblies().AsParallel()
+                from t in a.GetTypes()
+                let attributes = t.GetCustomAttributes(typeof(SourceGenerators.Attributes.AdapterAttribute), true)
+                where attributes != null && attributes.Length > 0
+                select new { Type = t, Attributes = attributes.Cast<SourceGenerators.Attributes.AdapterAttribute>() };
+            
+            foreach (var type in typesWithMyAttribute)
+            {
+
+            }
+        }
+    }
+}
---
+++ helpers/SourceGenerators/Attributes/SqlParamAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace SourceGenerators.Attributes
+{
+    [System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = true)]
+    public class SqlParamAttribute : System.Attribute
+    {
+        public string Name { get; }
+
+        public SqlParamAttribute(string name) => this.Name = name;
+    }
+}
