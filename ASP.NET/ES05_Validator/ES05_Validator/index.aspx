eb85feb4a518bd8d461ab982ac58757d8bfcbe98
Lorenzo Marchisio <l.marchisio.2268@vallauri.edu>
2023-10-24 11:34:24 +0200

    validators

---
+++ ASP.NET/ES05_Validator/ES05_Validator/index.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="ES05_Validator.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Label ID="Label1" runat="server" Text="Required Validator"></asp:Label>
+            <asp:TextBox ID="txtRequired" runat="server"></asp:TextBox>
+            
+            <br />
+
+            <asp:RequiredFieldValidator
+                ID="RequiredFieldValidator1"
+                runat="server"
+                ErrorMessage="RequiredFieldValidator"
+                ControlToValidate="txtRequired"
+                Display="Dynamic">
+
+            </asp:RequiredFieldValidator>
+
+            <hr />
+
+            <asp:Label ID="Label2" runat="server" Text="Range Validator"></asp:Label>
+            <asp:TextBox ID="txtRange" runat="server"></asp:TextBox>
+
+            <asp:RangeValidator
+                ID="RangeValidator1"
+                runat="server" 
+                ErrorMessage="RangeValidator"
+                ControlToValidate="txtRange"
+                MinimumValue="1"
+                MaximumValue="100"
+                Type="Integer">
+
+            </asp:RangeValidator>
+            <br />
+
+            <hr />
+
+            <asp:Button ID="btnSubmit" Text="Send" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/ES05_Validator/ES05_Validator/index.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Questo codice è stato generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ES05_Validator
+{
+
+
+    public partial class index
+    {
+
+        /// <summary>
+        /// Controllo form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controllo Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Controllo txtRequired.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRequired;
+
+        /// <summary>
+        /// Controllo RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
+        /// Controllo Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Controllo txtRange.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRange;
+
+        /// <summary>
+        /// Controllo RangeValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator RangeValidator1;
+
+        /// <summary>
+        /// Controllo btnSubmit.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSubmit;
+    }
+}
